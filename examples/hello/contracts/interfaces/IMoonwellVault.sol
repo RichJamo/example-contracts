d0627177f93587e4a7071b05430ca21ac0456fc2
Richard Jamieson <jamieson.rich@gmail.com>
2024-10-10 10:28:16 +0200

    Add return values to deposit and withdraw

--- examples/hello/contracts/interfaces/IMoonwellVault.sol
+++ examples/hello/contracts/interfaces/IMoonwellVault.sol
@@ -2,8 +2,18 @@
 pragma solidity 0.8.26;
 
 interface IMoonwellVault {
-    function deposit(uint256 assets, address receiver) external;
-    function withdraw(uint256 assets, address receiver, address owner) external;
+    function deposit(
+        uint256 assets,
+        address receiver
+    ) external returns (uint256);
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external returns (uint256);
+
     function balanceOf(address account) external view returns (uint256);
+
     function convertToAssets(uint256 shares) external view returns (uint256);
 }

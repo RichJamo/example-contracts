4357f3c81ce658a7ec451760a48cd7f93e18705a
Richard Jamieson <jamieson.rich@gmail.com>
2024-10-31 12:43:39 +0200

    Add base sepolia network and key

--- examples/hello/hardhat.config.ts
+++ examples/hello/hardhat.config.ts
@@ -8,13 +8,20 @@ import "@zetachain/localnet/tasks";
 import "@nomicfoundation/hardhat-toolbox";
 import "@zetachain/toolkit/tasks";
 import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
 
 import { getHardhatConfigNetworks } from "@zetachain/networks";
 import { HardhatUserConfig } from "hardhat/config";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   networks: {
     ...getHardhatConfigNetworks(),
+    base_sepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`, // or equivalent provider URL
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Add your private key here
+    },
   },
   solidity: {
     compilers: [
